6e6bcda2c6152166041d12d8b86911c8609c148e
crypt0 <acvjetan@gmail.com>
2020-12-12 11:28:33 +0100

    Adding custom directives.

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="show-blogs">
+  <div v-theme:column="'wide'" id="show-blogs">
     <h1>All Blog Articles</h1>
     <div :key="blog.id" v-for="blog in blogs" class="single-blog">
       <h2 v-rainbow>{{ blog.title }}</h2>
@@ -10,32 +10,32 @@
 
 <script>
 export default {
-data(){
-  return{
-    blogs: []
-  }
-},
-methods: {
-
-},
-created() {
-  this.$http.get('https://jsonplaceholder.typicode.com/posts').then((data) => {
-    this.blogs = data.body.slice(0,10);
-  })
-}
-}
+  data() {
+    return {
+      blogs: [],
+    };
+  },
+  methods: {},
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => {
+        this.blogs = data.body.slice(0, 10);
+      });
+  },
+};
 </script>
 
 <style>
- #show-blogs {
-   max-width: 800px;
-   margin: 0 auto;
- }
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
 
- .single-blog {
-   padding: 20px;
-   margin: 20px 0;
-   box-sizing: border-box;
-   background: #eee;
- }
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
 </style>
